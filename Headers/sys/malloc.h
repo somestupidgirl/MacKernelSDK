e31a7bd0d9de8d7efe9c2dbd9c0ee1ebe14e1398
somestupidgirl <sunnevanattsol@gmail.com>
2022-04-11 15:52:33 +0000

    Headers: sys/malloc.h: Expand the malloc header to contain the private KPI types as well
    Also added new macros for compatibility with FreeBSD/NetBSD malloc() and free()
    so they don't have to be manually replaced each time when porting over code.

--- Headers/sys/malloc.h
+++ Headers/sys/malloc.h
@@ -72,28 +72,68 @@
 #define _SYS_MALLOC_H_
 
 #include <sys/appleapiopts.h>
-
+#include <sys/types.h>
 
 /*
  * flags to malloc
  */
-#define M_WAITOK        0x0000
-#define M_NOWAIT        0x0001
-#define M_ZERO          0x0004          /* bzero the allocation */
-#define M_NULL          0x0008          /* return NULL if space is unavailable*/
+#define M_WAITOK                0x0000
+#define M_NOWAIT                0x0001
+#define M_ZERO                  0x0004  /* bzero the allocation */
+#define M_NULL                  0x0008  /* return NULL if space is unavailable*/
 
-
-#define M_RTABLE        5       /* routing tables */
-#define M_IFADDR        9       /* interface address (IOFireWireIP)*/
-#define M_LOCKF         40      /* Byte-range locking structures (msdos) */
-#define M_TEMP          80      /* misc temporary data buffers */
-#define M_KAUTH         100     /* kauth subsystem (smb) */
-#define M_SONAME        11      /* socket name (smb) */
-#define M_PCB           4       /* protocol control block (smb) */
-#define M_UDFNODE       84      /* UDF inodes (udf)*/
-#define M_UDFMNT        85      /* UDF mount structures (udf)*/
-
-
+/*
+ * Types of memory to be allocated (not all are used by us)
+ */
+#define M_DEVBUF                2       /* device driver memory */
+#define M_PCB                   4       /* protocol control block */
+#define M_RTABLE                5       /* routing tables */
+#define M_IFADDR                9       /* interface address */
+#define M_SONAME                11      /* socket name */
+#define M_PGRP                  17      /* process group header */
+#define M_FHANDLE               21      /* network file handle */
+#define M_NFSNODE               24      /* NFS vnode private part */
+#define M_VNODE                 25      /* Dynamically allocated vnodes */
+#define M_CACHE                 26      /* Dynamically allocated cache entries */
+#define M_DQUOT                 27      /* UFS quota entries */
+#define M_PROC_UUID_POLICY      28      /* proc UUID policy entries */
+#define M_SHM                   29      /* SVID compatible shared memory segments */
+#define M_LOCKF                 40      /* Byte-range locking structures */
+#define M_PROC                  41      /* Proc structures */
+#define M_NETADDR               49      /* Export host address structure */
+#define M_NFSSVC                50      /* NFS server structure */
+#define M_NFSD                  52      /* NFS server daemon structure */
+#define M_IPMOPTS               53      /* internet multicast options */
+#define M_IFMADDR               55      /* link-level multicast address */
+#define M_NFSBIO                58      /* NFS client I/O buffers */
+#define M_NFSBIGFH              61      /* NFS version 3 file handle */
+#define M_TTYS                  65      /* allocated tty structures */
+#define M_OFILETABL             73      /* Open file descriptor table */
+#define M_TEMP                  80      /* misc temporary data buffers */
+#define M_SECA                  81      /* security associations, key management */
+#define M_DEVFS                 82
+#define M_UDFNODE               84      /* UDF inodes */
+#define M_UDFMNT                85      /* UDF mount structures */
+#define M_IP6OPT                87      /* IPv6 options management */
+#define M_KQUEUE                94      /* kqueue system */
+#define M_KAUTH                 100     /* kauth subsystem */
+#define M_DUMMYNET              101     /* dummynet */
+#define M_MACTEMP               104     /* MAC framework */
+#define M_SBUF                  105     /* string buffers */
+#define M_SELECT                107     /* per-thread select memory */
+#define M_INMFILTER             110     /* IPv4 multicast PCB-layer source filter */
+#define M_IN6MFILTER            112     /* IPv6 multicast PCB-layer source filter */
+#define M_IP6MOPTS              113     /* IPv6 multicast options */
+#define M_IP6CGA                117
+#define M_NECP                  118     /* General NECP policy data */
+#define M_FD_VN_DATA            122     /* Per fd vnode data */
+#define M_FD_DIRBUF             123     /* Directory entries' buffer */
+#define M_NETAGENT              124     /* Network Agents */
+#define M_EVENTHANDLER          125     /* Eventhandler */
+#define M_LLTABLE               126     /* Link layer table */
+#define M_NWKWQ                 127     /* Network work queue */
+#define M_CFIL                  128     /* Content Filter */
+#define M_LAST                  129     /* Must be last type + 1 */
 
 /*
  * The malloc/free primatives used
@@ -101,46 +141,51 @@
  */
 
 #define MALLOC(space, cast, size, type, flags) \
-	(space) = (cast)_MALLOC(size, type, flags)
+    (space) = (cast)_MALLOC(size, type, flags)
 
 #define FREE(addr, type) \
-	_FREE((void *)addr, type)
+    _FREE((void *)addr, type)
 
 #define REALLOC(space, cast, addr, size, type, flags) \
-	(space) = (cast)_REALLOC(addr, size, type, flags)
+    (space) = (cast)_REALLOC(addr, size, type, flags)
 
 #define MALLOC_ZONE(space, cast, size, type, flags) \
-	(space) = (cast)_MALLOC_ZONE(size, type, flags)
+    (space) = (cast)_MALLOC_ZONE(size, type, flags)
 
 #define FREE_ZONE(addr, size, type) \
-	_FREE_ZONE((void *)addr, size, type)
+    _FREE_ZONE((void *)addr, size, type)
 
 extern void     *_MALLOC(
-	size_t          size,
-	int             type,
-	int             flags);
+    size_t          size,
+    int             type,
+    int             flags);
 
 extern void     _FREE(
-	void            *addr,
-	int             type);
+    void            *addr,
+    int             type);
 
 extern void     *_REALLOC(
-	void            *addr,
-	size_t          size,
-	int             type,
-	int             flags);
+    void            *addr,
+    size_t          size,
+    int             type,
+    int             flags);
 
 extern void     *_MALLOC_ZONE(
-	size_t          size,
-	int             type,
-	int             flags);
+    size_t          size,
+    int             type,
+    int             flags);
 
 extern void     _FREE_ZONE(
-	void            *elem,
-	size_t          size,
-	int             type);
+    void            *elem,
+    size_t          size,
+    int             type);
 
-
-
+/*
+ * Note: Net/FreeBSD-compatible malloc() and free() to make porting less of a hassle.
+ *
+ *       - https://github.com/somestupidgirl/
+ */
+#define malloc(size, type, flags)    _MALLOC(size, type, flags)
+#define free(addr, type)             _FREE(addr, type)
 
 #endif  /* _SYS_MALLOC_H_ */
